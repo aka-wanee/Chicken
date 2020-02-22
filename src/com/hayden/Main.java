a1cae71fa4bdaac51684f63a0dab5b82ce71d1bc
aka-wanee <iamtheonetobe@gmail.com>
2020-02-22 20:28:47 +0900

    2주차 ver1. - 브랜드를 선택 시 메뉴가 출력 - 메뉴를 선택하면 치킨 아스키 아트와 함께 배달완료 메시지 출력(따로 메소드 분리)

--- src/com/hayden/Main.java
+++ src/com/hayden/Main.java
@@ -2,147 +2,91 @@ package com.hayden;
 
 import java.util.Scanner;
 
+
 public class Main {
+
+
     public static void main(String[] args) {
-        int choice = -1;
         boolean condition = true;
 
-        String brands[] = {"BHC","DDORAE ORAE","GOOBNE","YES YES","KFC","HOSICK'S 2 CHICKENS"};
-        String menus[] = {"양념","후라이드","반반무마니","갈릭","파닭","뿌링클", "핫크리스피"};
-
+        String[] brands = {"BHC", "DDORAE ORAE", "GOOBNE", "YES YES", "KFC", "HOSICK'S 2 CHICKENS"};
+        String[] menus = {"양념", "후라이드", "반반무마니", "갈릭", "파닭", "뿌링클", "핫크리스피"};
         String selectedBrand = "";
+
+        int choice = -1;
         int selectedMenu = -1;
-        //String menuOfBrand;
-        //String menuList[][];
+        int[][] menuList = {{0, 3, 5}, {1, 2, 3}, {2, 4, 6}, {1, 2, 5}, {0, 1, 3}};
 
-        Scanner scanner = new Scanner(System.in);
-
+        //첫 화면
         while (condition) {
+            Scanner scanner = new Scanner(System.in);
+
             System.out.println(
                     "오늘은 치킨이닭!" +
                             "\n어디에 전화를 걸까?" +
                             "\n1. B H C" +
                             "\n2. 또래오래" +
-                            "\n3. 굽  네" +
-                            "\n4. 네  네" +
-                            "\n5. K F C" +
-                            "\n6. 호식이 2마리 치킨" +
+                            "\n3. 네네치킨" +
+                            "\n4. K F C" +
+                            "\n5. 호식이 2마리 치킨" +
                             "\n\n원하는 번호를 입력하고 엔터를 눌러주세요."
             );
 
+            //선택한 메뉴 번호 입력받기
             choice = scanner.nextInt();
 
-            switch (choice) {
-                case 1:
-                    selectedBrand=brands[0];
-                    System.out.println("네 고객님~"+selectedBrand+" 어떤 메뉴 주문하시겠어요??"+
-                            "\n1)"+menus[0]+
-                            "\n2)"+menus[3]+
-                            "\n3)"+menus[5]);
-                    choice=scanner.nextInt();
-                    if(choice==1||choice==2||choice==3) {
-                        selectedMenu=choice;
-                    } else {
-                        System.out.println("고객님, 그 메뉴는 저희 가게에 없는데요..");
-                    }
-                    break;
-                case 2:
-                    selectedBrand=brands[1];
-                    System.out.println("네 고객님~"+selectedBrand+" 어떤 메뉴 주문하시겠어요??"+
-                            "\n1)"+menus[1]+
-                            "\n2)"+menus[2]+
-                            "\n3)"+menus[3]);
-                    choice=scanner.nextInt();
-                    if(choice==1||choice==2||choice==3) {
-                        selectedMenu=choice;
-                    } else {
-                        System.out.println("고객님, 그 메뉴는 저희 가게에 없는데요..");
-                    }
+            if (choice < 6) {
+                //선택된 브랜드 이름 받아오기
+                selectedBrand = brands[choice - 1];
 
-                    break;
-                case 3:
-                    selectedBrand=brands[2];
-                    System.out.println("네 고객님~"+selectedBrand+" 어떤 메뉴 주문하시겠어요??"+
-                            "\n1)"+menus[2]+
-                            "\n2)"+menus[4]+
-                            "\n3)"+menus[6]);
-                    choice=scanner.nextInt();
-                    if(choice==1||choice==2||choice==3) {
-                        selectedMenu=choice;
-                    } else {
-                        System.out.println("고객님, 그 메뉴는 저희 가게에 없는데요..");
-                    }
-                    break;
-                case 4:
-                    selectedBrand=brands[3];
-                    System.out.println("네 고객님~"+selectedBrand+" 어떤 메뉴 주문하시겠어요??"+
-                            "\n1)"+menus[3]+
-                            "\n2)"+menus[4]+
-                            "\n3)"+menus[6]);
-                    choice=scanner.nextInt();
-                    if(choice==1||choice==2||choice==3) {
-                        selectedMenu=choice;
-                    } else {
-                        System.out.println("고객님, 그 메뉴는 저희 가게에 없는데요..");
-                    }
-                    break;
-                case 5:
-                    selectedBrand=brands[4];
-                    System.out.println("네 고객님~"+selectedBrand+" 어떤 메뉴 주문하시겠어요??"+
-                            "\n1)"+menus[1]+
-                            "\n2)"+menus[2]+
-                            "\n3)"+menus[5]);
-                    choice=scanner.nextInt();
-                    if(choice==1||choice==2||choice==3) {
-                        selectedMenu=choice;
-                    } else {
-                        System.out.println("고객님, 그 메뉴는 저희 가게에 없는데요..");
-                    }
-                    break;
-                case 6:
-                    selectedBrand=brands[5];
-                    System.out.println("네 고객님~"+selectedBrand+" 어떤 메뉴 주문하시겠어요??"+
-                            "\n1)"+menus[1]+
-                            "\n2)"+menus[2]+
-                            "\n3)"+menus[3]);
-                    choice=scanner.nextInt();
-                    if(choice==1||choice==2||choice==3) {
-                        selectedMenu=choice;
-                    } else {
-                        System.out.println("고객님, 그 메뉴는 저희 가게에 없는데요..");
-                    }
-                    break;
+                //해당 브랜드의 메뉴 출력하기
+                for (int i = 0; i < 3; i++) {
+                    System.out.println((i + 1) + ") " + menus[menuList[choice - 1][i]]);
+                }
 
-                default:
-                    System.out.println("메뉴에 있는 브랜드만 선택 가능합니다.");
-                    break;
+                //선택한 메뉴 받아오기
+                choice = scanner.nextInt();
+
+                if (choice == 1 || choice == 2 || choice == 3) {
+                    selectedMenu = choice;
+                    System.out.println("띵동~♬ 주문하신 " + selectedBrand + " " + menus[choice] + " 배달왔습니다~");
+                    delicious();
+                    System.out.println("고객님~ 맛있게 드셨나요?");
+                } else {
+                    System.out.println("고객님, 그 메뉴는 저희 가게에 없는데요..");
+                }
+            } else {
+                System.out.println("1~5번까지만 선택 가능합니다");
             }
-            System.out.println("띵동~♬ 주문하신"+selectedBrand+" "+menus[choice]+" 배달왔습니다~");
-            delicious();
-            System.out.println("고객님~ 맛있게 드셨나요? 다시 주문하시겠어요? 1)한다 2)안 한다");
+
+            //처음으로 되돌아갈지 물어보는 질문
+            System.out.println("다시 주문하시겠어요? 1)한다 2)안 한다");
+
             if (scanner.nextInt() == 2) {
                 condition = false;
             }
 
+            scanner.close();
         }
-        scanner.close();
     }
 
+
+    //주문 시 치킨 이미지 출력
     public static void delicious() {
-                    System.out.println(
-                            "   .-----.\n" +
-                            " .~~--~-----.\n" +
-                            " -~~~~~~~~----.\n" +
-                            ".~~~~~~~~-------.\n" +
-                            "-~~~---~~~~~----~~,.\n" +
-                            ",~~~~~~~~--~~~---,.\n" +
-                            ",~~~~~~----~~~---~~\n" +
-                            "   -~~-~~~~----~~,,,\n" +
-                            "    ..,,,,----~~~~~~::\n" +
-                            "              -~~--~~~.\n" +
-                            "               ,----~~~\n" +
-                            "                 ~~~~--,"
-                    );
+        System.out.println(
+                "   .-----.\n" +
+                        " .~~--~-----.\n" +
+                        " -~~~~~~~~----.\n" +
+                        ".~~~~~~~~-------.\n" +
+                        "-~~~---~~~~~----~~,.\n" +
+                        ",~~~~~~~~--~~~---,.\n" +
+                        ",~~~~~~----~~~---~~\n" +
+                        "   -~~-~~~~----~~,,,\n" +
+                        "    ..,,,,----~~~~~~::\n" +
+                        "              -~~--~~~.\n" +
+                        "               ,----~~~\n" +
+                        "                 ~~~~--,"
+        );
     }
 
 }
